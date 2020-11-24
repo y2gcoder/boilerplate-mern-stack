71074f322ad1683fa35981f13cabcc15861e5cc7
Yang <y2gcoder@gmail.com>
2020-11-24 12:00:56 +0900

    #5 Multer로 노드 서버에 비디오 저장하기

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Typography, Button, Form, message, Input, Icon } from 'antd'
 import Dropzone from 'react-dropzone'
+import Axios from 'axios';
 
 const { TextArea } = Input;
 const { Title } = Typography;
@@ -38,6 +39,24 @@ function VideoUploadPage() {
         setCategory(e.currentTarget.value)
     }
 
+    const onDrop = (files) => {
+        //파일을 보낼 때 필요한 설정.
+        let formData = new FormData;
+        const config = {
+            header: {'content-type': 'multipart/form-data'}
+        }
+        formData.append('file', files[0])
+
+        Axios.post('/api/video/uploadfiles', formData, config)
+            .then(response => {
+                if (response.data.success) {
+                    console.log(response.data)
+                } else {
+                    alert('비디오 업로드를 실패했습니다.')
+                }
+            })
+    }
+
     return (
         <div style={{ maxWidth:'700px', margin:'2rem auto' }}>
             <div style={{ textAlign: 'center', marginBottom:'2rem' }}>
@@ -47,7 +66,7 @@ function VideoUploadPage() {
                 <div style={{ display:'flex', justifyContent: 'space-between' }} >
                     {/* Drop zone */}
 
-                    <Dropzone onDrop multiple maxSize>
+                    <Dropzone accept="video/*" onDrop={onDrop} multiple={false} maxSize={800000000}>
                         {({getRootProps, getInputProps}) => (
                             <div style={{ width:'300px', height: '240px', border:'1px solid lightgray'
                             , display:'flex', alignItems:'center', justifyContent: 'center'}} {...getRootProps()} >
