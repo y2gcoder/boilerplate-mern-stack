2eee79cae2b999b4811effdc2ff7b9106fabd645
Yang <y2gcoder@gmail.com>
2020-11-24 10:49:45 +0900

    #4 비디오 업로드 FORM 만들기 2

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Typography, Button, Form, message, Input, Icon } from 'antd'
 import Dropzone from 'react-dropzone'
 
 const { TextArea } = Input;
 const { Title } = Typography;
+
+const PrivateOptions = [
+    {value: 0, label: "Private"},
+    {value: 1, label: "Public"},
+]
+const CategoryOptions = [
+    { value: 0, label: "Film & Animation"},
+    { value: 1, label: "Autos & Vehicles"},
+    { value: 2, label: "Music"},
+    { value: 3, label: "Pets & Animals"},
+]
 function VideoUploadPage() {
+
+    const [VideoTitle, setVideoTitle] = useState("")
+    const [Description, setDescription] = useState("")
+    const [Private, setPrivate] = useState(0)
+    const [Category, setCategory] = useState("Film & Animation")
+
+    const onTitleChange = (e) => {
+        setVideoTitle(e.currentTarget.value)
+    }
+
+    const onDescriptionChange = (e) => {
+        setDescription(e.currentTarget.value)
+    }
+
+    const onPrivateChange = (e) => {
+        setPrivate(e.currentTarget.value)
+    }
+    
+    const onCategoryChange = (e) => {
+        setCategory(e.currentTarget.value)
+    }
+
     return (
         <div style={{ maxWidth:'700px', margin:'2rem auto' }}>
             <div style={{ textAlign: 'center', marginBottom:'2rem' }}>
@@ -33,20 +66,25 @@ function VideoUploadPage() {
                 <br/>
                 <br/>
                 <label>Title</label>
-                <Input onChange value/>
+                <Input onChange={onTitleChange} value={VideoTitle} />
                 <br/>
                 <br/>
                 <label>Description</label>
-                <TextArea onChange value />
+                <TextArea onChange={onDescriptionChange} value={Description} />
                 <br/>
                 <br/>
-                <select onChange>
-                    <option key value></option>
+                <select onChange={onPrivateChange}>
+                    {/* map은 항상 key가 있어야 에러 메시지가 나오지 않음. */}
+                    {PrivateOptions.map((item, index) => (
+                        <option key={index} value={item.value}>{item.label}</option>
+                    ))}
                 </select>
                 <br/>
                 <br/>
-                <select onChange>
-                    <option key value></option>
+                <select onChange={onCategoryChange}>
+                    {CategoryOptions.map((item, index) => (
+                        <option key={index} value={item.value}>{item.label}</option>
+                    ))}
                 </select>
                 <br/>
                 <br/>
