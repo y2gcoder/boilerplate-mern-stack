962328876f7f9cfacf980f6cf2f66e2870d8f499
Yang <y2gcoder@gmail.com>
2020-12-15 16:34:44 +0900

    #20 좋아요 싫어요 기능 (3) 클릭시 기능들

--- client/src/components/views/VideoDetailPage/Sections/LikeDisLikes.js
+++ client/src/components/views/VideoDetailPage/Sections/LikeDisLikes.js
@@ -53,27 +53,85 @@ function LikeDisLikes(props) {
         })
     }, [])
 
+    const onLike = () => {
+        if (LikeAction === null ) {
+
+            Axios.post('/api/like/upLike', variable)
+                .then(response => {
+                    if (response.data.success) {
+                        setLikes(Likes + 1);
+                        setLikeAction("liked")
+
+                        if (DislikeAction !== null) {
+                            setDislikeAction(null);
+                            setDislikes(Dislikes - 1);
+                        }
+                    } else {
+                        alert("Like를 올리지 못했습니다.")
+                    }
+                })
+        } else {
+            Axios.post('/api/like/unLike', variable)
+                .then(response => {
+                    if (response.data.success) {
+                        setLikes(Likes - 1);
+                        setLikeAction(null);
+                    } else {
+                        alert("Like를 내리지 못했습니다.")
+                    }
+                })
+        }
+    }
+
+    const onDislike = () => {
+        if (DislikeAction !== null) {
+            Axios.post('/api/like/unDislike', variable)
+                .then(response => {
+                    if (response.data.success) {
+                        setDislikes(Dislikes - 1);
+                        setDislikeAction(null);
+                    } else {
+                        alert('dislike을 지우지 못했습니다.')
+                    }
+                })
+        } else {
+            Axios.post("/api/like/upDislike", variable)
+                .then(response => {
+                    if (response.data.success) {
+                        setDislikes(Dislikes + 1);
+                        setDislikeAction("disliked")
+                        if (LikeAction !== null){
+                            setLikeAction(null)
+                            setLikes(Likes - 1);
+                        }
+                    } else {
+                        alert('dislike을 지우지 못했습니다.')
+                    }
+                })
+        }
+    }
+
     return (
         <div>
             <span key="comment-basic-like">
                 <Tooltip title="Like">
                     <Icon type="like"
                         theme={LikeAction === 'liked' ? "filled" : "outlined"}
-                        onClick
+                        onClick={onLike}
                     />
                 </Tooltip>
                 <span style={{ paddingLeft: '8px', cursor: 'auto'}}> {Likes} </span>
-            </span>
+            </span>&nbsp;&nbsp;
 
             <span key="comment-basic-dislike">
                 <Tooltip title="Dislike">
                     <Icon type="dislike"
                         theme={DislikeAction === 'disliked' ? "filled" : "outlined"}
-                        onClick
+                        onClick={onDislike}
                     />
                 </Tooltip>
                 <span style={{ paddingLeft: '8px', cursor: 'auto'}}> {Dislikes} </span>
-            </span>
+            </span>&nbsp;&nbsp;
         </div>
     )
 }
--- server/routes/like.js
+++ server/routes/like.js
@@ -39,4 +39,78 @@ router.post("/getDislikes", (req, res) => {
         })
 })
 
+router.post("/upLike", (req, res) => {
+    let variable = {}
+
+    if (req.body.videoId) {
+        variable = { videoId: req.body.videoId, userId: req.body.userId }
+    } else {
+        variable = { commentId: req.body.commentId, userId: req.body.userId }
+    }
+    //Like collection에다가 click 정보를 넣어줌
+    const like = new Like(req.body);
+    like.save((err, likeResult) => {
+        if (err) return res.json({ success: false, err })
+
+        // 만약에 Dislike이 이미 클릭이 되어있다면, Dislike을 1 줄여준다.
+        Dislike.findOneAndDelete(variable)
+            .exec((err, dislikeResult) => {
+                if (err) return res.status(400).json({ successs: false, err })
+                res.status(200).json({ success: true })
+            })
+    })
+})
+
+router.post("/unLike", (req, res) => {
+    let variable = {}
+
+    if (req.body.videoId) {
+        variable = { videoId: req.body.videoId, userId: req.body.userId }
+    } else {
+        variable = { commentId: req.body.commentId, userId: req.body.userId }
+    }
+    Like.findOneAndDelete(variable)
+        .exec((err, result) => {
+            if (err) return res.status(400).json({ success: false, err })
+            res.status(200).json({ success: true })
+        })
+})
+
+router.post("/unDislike", (req, res) => {
+    let variable = {}
+
+    if (req.body.videoId) {
+        variable = { videoId: req.body.videoId, userId: req.body.userId }
+    } else {
+        variable = { commentId: req.body.commentId, userId: req.body.userId }
+    }
+    Dislike.findOneAndDelete(variable)
+        .exec((err, result) => {
+            if (err) return res.status(400).json({ success: false, err })
+            res.status(200).json({ success: true })
+        })
+})
+
+router.post("/upDislike", (req, res) => {
+    let variable = {}
+
+    if (req.body.videoId) {
+        variable = { videoId: req.body.videoId, userId: req.body.userId }
+    } else {
+        variable = { commentId: req.body.commentId, userId: req.body.userId }
+    }
+    //Dislike collection에다가 click 정보를 넣어줌
+    const dislike = new Dislike(req.body);
+    dislike.save((err, dislikeResult) => {
+        if (err) return res.json({ success: false, err })
+
+        // 만약에 Like이 이미 클릭이 되어있다면, Like을 1 줄여준다.
+        Like.findOneAndDelete(variable)
+            .exec((err, likeResult) => {
+                if (err) return res.status(400).json({ successs: false, err })
+                res.status(200).json({ success: true })
+            })
+    })
+})
+
 module.exports = router;
