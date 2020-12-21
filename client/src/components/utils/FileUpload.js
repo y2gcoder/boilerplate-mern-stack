8afdb85243e1d4b725a9983c2566e9ed5920b787
Yang <y2gcoder@gmail.com>
2020-12-21 11:51:55 +0900

    #7 이미지 파일을 서버로 보내기

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import Dropzone from 'react-dropzone'
 import { Icon } from 'antd'
+import axios from 'axios'
 
 function FileUpload() {
+    const formData = new FormData();
+    const config = {
+        header: { 'content-type': 'multipart/form-data' }
+    }
+    formData.append('file', files[0])
+
+    const dropHandler = (files) => {
+        axios.post('/api/product/image', formData, config)
+            .then(response => {
+                if (response.data.success) {
+                    
+                } else {
+                    alert('파일을 저장하는데 실패했습니다.')
+                }
+            })
+    }
+
     return (
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+            <Dropzone onDrop={dropHandler}>
                 {({ getRootProps, getInputProps }) => (
-                    <div 
-                        style={{ 
-                            width:300, height: 240, border: '1px solid lightgray',
+                    <div
+                        style={{
+                            width: 300, height: 240, border: '1px solid lightgray',
                             display: 'flex', alignItems: 'center', justifyContent: 'center'
-                         }}
+                        }}
                         {...getRootProps()}>
                         <input {...getInputProps()} />
                         <Icon type="plus" style={{ fontSize: '3rem' }} />
---
+++ server/routes/product.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+//=================================
+//             Product
+//=================================
+
+router.post('/image', (req, res) => {
+    //가져온 이미지를 저장하면 됨.
+    
+})
+
+module.exports = router;
