eb55035b42665bf9645cead8e3b31462b27130ea
Yang <y2gcoder@gmail.com>
2020-12-21 13:20:51 +0900

    #8 multer를 이용하여 이미지 저장

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Dropzone from 'react-dropzone'
 import { Icon } from 'antd'
 import axios from 'axios'
 
 function FileUpload() {
-    const formData = new FormData();
-    const config = {
-        header: { 'content-type': 'multipart/form-data' }
-    }
-    formData.append('file', files[0])
+
+    const [Images, setImages] = useState([])
 
     const dropHandler = (files) => {
+        const formData = new FormData();
+        const config = {
+            header: { 'content-type': 'multipart/form-data' }
+        }
+        formData.append('file', files[0])
         axios.post('/api/product/image', formData, config)
             .then(response => {
                 if (response.data.success) {
-                    
+                    console.log(response.data);
+                    setImages([...Images, response.data.filePath])
                 } else {
                     alert('파일을 저장하는데 실패했습니다.')
                 }
@@ -36,6 +39,17 @@ function FileUpload() {
                     </div>
                 )}
             </Dropzone>
+
+            {/* UI */}
+            <div style={{display: 'flex', width: '350px', height: '240px', overflowX: 'scroll' }} >
+                        {Images.map((image, index) => (
+                            <div key={index}>
+                                <img style={{ minWidth: '300px', width: '300px', height: '240px '}}
+                                    src={`http://localhost:5000/${image}`}
+                                />
+                            </div>
+                        ))}
+            </div>
         </div>
     )
 }
