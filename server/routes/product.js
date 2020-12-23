56e3838d2c861552b7a40ef2ad1abf45240f4a03
Yang <y2gcoder@gmail.com>
2020-12-23 09:33:49 +0900

    #12 데이터베이스에 들어 있는 모든 상품을 가져오기

--- server/routes/product.js
+++ server/routes/product.js
@@ -37,4 +37,14 @@ router.post('/', (req, res) => {
     })
 })
 
+router.post('/products', (req, res) => {
+    //product collection에 들어있는 모든 상품 정보를 가져오기
+    Product.find()
+        .populate('writer')
+        .exec((err, productsInfo) => {
+            if (err) return res.status(400).json({ success: false, err })
+            return res.status(200).json({ success: true, productsInfo})
+        })
+})
+
 module.exports = router;
