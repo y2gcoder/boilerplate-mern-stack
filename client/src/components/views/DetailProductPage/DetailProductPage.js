b0c4ade3558b2ef61610befa1cc6b5388bf4416a
y2gcoder <y2gcoder@gmail.com>
2021-01-13 09:28:30 +0900

    feat: create Product Info component

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -34,7 +34,7 @@ function DetailProductPage(props) {
                 </Col>
                 <Col lg={12} sm={24}>
                     {/* ProductInfo */}
-                    <ProductInfo />
+                    <ProductInfo detail={Product} />
                 </Col>
             </Row>
 
