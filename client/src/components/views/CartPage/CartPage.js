394a16d0ba274c7892186bc424a8ba1c36ec003b
y2gcoder <y2gcoder@gmail.com>
2021-01-13 11:33:53 +0900

    feat: 데이터베이스에서 가져온 상품 정보들을 화면에서 보여주기

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { getCartItems } from '../../../_actions/user_actions'
+import UserCardBlock from './Sections/UserCardBlock'
 
 function CartPage(props) {
     const dispatch = useDispatch();
@@ -21,8 +22,12 @@ function CartPage(props) {
     }, [props.user.userData])
 
     return (
-        <div>
-            CartPage
+        <div style={{ width: '85%', margin: '3rem auto' }}>
+            <h1>My Cart</h1>
+            <div>
+                <UserCardBlock products={props.user.cartDetail && props.user.cartDetail.product} />
+            </div>
+
         </div>
     )
 }
