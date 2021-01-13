73629dd849df8b044d67c422320e432cdc8cc8f9
y2gcoder <y2gcoder@gmail.com>
2021-01-13 13:58:39 +0900

    feat: 카트에 있는 모든 상품 지운 다음 처리할 것들

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { getCartItems, removeCartItem } from '../../../_actions/user_actions'
 import UserCardBlock from './Sections/UserCardBlock'
+import { Empty } from 'antd'
 
 function CartPage(props) {
     const dispatch = useDispatch();
 
     const [Total, setTotal] = useState(0)
+    const [ShowTotal, setShowTotal] = useState(false)
     useEffect(() => {
 
         const cartItems = [];
@@ -31,12 +33,15 @@ function CartPage(props) {
         })
 
         setTotal(total);
+        setShowTotal(true);
     }
 
     const removeFromCart = (productId) => {
         dispatch(removeCartItem(productId))
             .then(response => {
-
+                if (response.payload.productInfo.length <= 0) {
+                    setShowTotal(false);
+                }
             })
     }
 
@@ -46,9 +51,19 @@ function CartPage(props) {
             <div>
                 <UserCardBlock products={props.user.cartDetail} removeItem={removeFromCart} />
             </div>
-            <div style={{ marginTop: '3rem' }}>
-                <h2>Total Amount: ${Total}</h2>
-            </div>
+
+            {ShowTotal ? 
+                <div style={{ marginTop: '3rem' }}>
+                    <h2>Total Amount: ${Total}</h2>
+                </div>
+                :
+                <>
+                    <br/>
+                    <Empty description={false} />
+                </>
+                
+            }
+            
         </div>
     )
 }
